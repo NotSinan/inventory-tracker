6e8f38d457717b81a78ae6a71e39178ed24fa5d9
sinan <haxsmith77@gmail.com>
2023-04-19 03:49:37 +1000

    Added sale.py

---
+++ sale.py
@@ -0,0 +1,6 @@
+class Sale:
+    def __init__(self, item_name, quantity, total_price) -> None:
+        self.item_name = item_name
+        self.quantity = quantity
+        self.total_price = total_price
+        
