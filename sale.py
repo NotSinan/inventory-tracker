ad2212bc34831f39c3e2c3980363b48e90b5b43e
sinan <haxsmith77@gmail.com>
2023-04-19 23:06:01 +1000

    Updated sale.py

--- sale.py
+++ sale.py
@@ -1,6 +1,8 @@
+import datetime
 class Sale:
     def __init__(self, item_name, quantity, total_price) -> None:
         self.item_name = item_name
         self.quantity = quantity
         self.total_price = total_price
+        self.date = datetime.datetime.now()
         
